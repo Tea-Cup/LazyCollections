66e49e8593321dcb1b23adcaade9c9dfd49aecea
Tea-Cup <CozyTeaCup@pm.me>
2019-08-16 14:54:05 +0700

    enumerable tests

--- tests.cpp
+++ tests.cpp
@@ -18,3 +18,26 @@ TEST_CASE("Exactly 5 iteration on 5 long array") {
   REQUIRE(en.moveNext());
   REQUIRE_FALSE(en.moveNext());
 }
+
+TEST_CASE("For adapter working") {
+  int arr[5] = {1,2,3,4,5};
+  lazycoll::enumerable<int> en(arr, 5);
+  int iterations = 0;
+  for(int i: en) {
+    REQUIRE(i == arr[iterations++]);
+  }
+}
+
+TEST_CASE("Throws when out of bounds") {
+  int arr[1] = {1};
+  lazycoll::enumerable<int> en(arr, 1);
+  REQUIRE(en.moveNext());
+  REQUIRE_FALSE(en.moveNext());
+  REQUIRE_THROWS(en.current());
+}
+
+TEST_CASE("Throws if not moved") {
+  int arr[1] = {1};
+  lazycoll::enumerable<int> en(arr, 1);
+  REQUIRE_THROWS(en.current());
+}
