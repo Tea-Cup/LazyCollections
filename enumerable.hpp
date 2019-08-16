203eb2c169b4e162765cb7f23181e1e327c76efa
Tea-Cup <CozyTeaCup@pm.me>
2019-08-16 16:34:54 +0700

    rename and format

--- enumerable.hpp
+++ enumerable.hpp
@@ -1,71 +1,58 @@
 #pragma once
 #ifndef LAZYCOLL_ENUMERABLE
 #define LAZYCOLL_ENUMERABLE
-#include <cstdint>
-#include <stdexcept>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <iterator>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 
 namespace lazycoll {
-  template<class T>
-  class iterator: std::iterator<std::forward_iterator_tag, T> {
-    T* array;
-    const std::size_t size;
-    std::size_t index;
-  public:
-    iterator(T* arr, const std::size_t size, const std::size_t index)
+
+template <class T>
+class Iterator : std::iterator<std::forward_iterator_tag, T> {
+  T* array;
+  const std::size_t size;
+  std::size_t index;
+
+ public:
+  Iterator(T* arr, const std::size_t size, const std::size_t index)
       : array(arr), size(size), index(index) {}
 
-    iterator& operator++() {
-      index++;
-      return *this;
-    }
-    iterator operator++(int) {
-      return iterator<T>(array, size, index++);
-    }
-    T& operator*() const {
-      return array[index];
-    }
-    T& operator->() const {
-      return array[index];
-    }
-    bool operator ==(const iterator& it) const {
-      return index == it.index;
-    }
-    bool operator !=(const iterator& it) const {
-      return index != it.index;
-    }
-  };
+  Iterator& operator++() {
+    index++;
+    return *this;
+  }
+  Iterator operator++(int) { return Iterator<T>(array, size, index++); }
+  T& operator*() const { return array[index]; }
+  T& operator->() const { return array[index]; }
+  bool operator==(const Iterator& it) const { return index == it.index; }
+  bool operator!=(const Iterator& it) const { return index != it.index; }
+};
 
-  template<typename T>
-  class enumerable {
-    private:
-      T* array;
-      std::size_t size;
-      std::size_t index;
-    public:
-      enumerable(T* arr, std::size_t size)
-        : array(arr), size(size), index(-1) {}
+template <typename T>
+class Enumerable {
+ private:
+  T* array;
+  std::size_t size;
+  std::size_t index;
 
-      T& current() const {
-        if(this->index < 0 || this->index >= this->size)
-          throw std::out_of_range("Index out of range");
-        return this->array[index];
-      }
+ public:
+  Enumerable(T* arr, std::size_t size) : array(arr), size(size), index(-1) {}
 
-      bool moveNext() {
-        return ++this->index < this->size;
-      }
+  T& current() const {
+    if (this->index < 0 || this->index >= this->size)
+      throw std::out_of_range("Index out of range");
+    return this->array[index];
+  }
 
-      iterator<T> begin() {
-        return iterator<T>(array, size, 0);
-      }
-      iterator<T> end() {
-        return iterator<T>(array, size, size);
-      }
-  };
-}
+  bool moveNext() { return ++this->index < this->size; }
+
+  Iterator<T> begin() { return Iterator<T>(array, size, 0); }
+  Iterator<T> end() { return Iterator<T>(array, size, size); }
+};
+
+}  // namespace lazycoll
 #endif
--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
   int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  lazycoll::enumerable<int> en(array, 10);
-  for(int i : en){
+  lazycoll::Enumerable<int> en(array, 10);
+  for (int i : en) {
     std::cout << i << std::endl;
   }
   return 0;
